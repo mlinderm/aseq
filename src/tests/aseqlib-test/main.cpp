3f4609320d05a134e6fd4a1f0dd84153d02903ba
Michael Linderman <michael.linderman@mssm.edu>
2015-12-12 15:54:22 -0500

    Integrate clang-format

--- src/tests/aseqlib-test/main.cpp
+++ src/tests/aseqlib-test/main.cpp
@@ -2,10 +2,10 @@
 #include <glog/logging.h>
 
 int main(int argc, char* argv[]) {
-    ::testing::InitGoogleMock(&argc, argv);
+  ::testing::InitGoogleMock(&argc, argv);
 
-    google::InitGoogleLogging(argv[0]);
-    FLAGS_logtostderr = 1;
+  google::InitGoogleLogging(argv[0]);
+  FLAGS_logtostderr = 1;
 
-    return RUN_ALL_TESTS();
+  return RUN_ALL_TESTS();
 }
